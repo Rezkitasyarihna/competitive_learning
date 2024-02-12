fb3cd236517531d0b18dd059afff2fb6a326e2b5
Rezkita Syarihna <120299952+Rezkitasyarihna@users.noreply.github.com>
2024-02-12 23:20:42 +0700

    Update recyclable-and-low-fat-products.py

--- 30-days-of-pandas/recyclable-and-low-fat-products.py
+++ 30-days-of-pandas/recyclable-and-low-fat-products.py
@@ -1,3 +1,5 @@
+# Write a solution to find the ids of products that are both low fat and recyclable.
+# Return the result table in any order.
 import pandas as pd
 
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
