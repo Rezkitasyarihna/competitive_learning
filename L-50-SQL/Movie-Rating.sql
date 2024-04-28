dc7b4ebe51e89a90d5fed6f2130b3a670cbc38dc
Rezkita Syarihna <120299952+Rezkitasyarihna@users.noreply.github.com>
2024-04-28 15:38:42 +0700

    Create Movie-Rating.sql

---
+++ L-50-SQL/Movie-Rating.sql
@@ -0,0 +1,74 @@
+-- Table: Movies
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | title         | varchar |
+-- +---------------+---------+
+-- movie_id is the primary key (column with unique values) for this table.
+-- title is the name of the movie.
+ 
+
+-- Table: Users
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+ 
+
+-- Table: MovieRating
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | user_id       | int     |
+-- | rating        | int     |
+-- | created_at    | date    |
+-- +---------------+---------+
+-- (movie_id, user_id) is the primary key (column with unique values) for this table.
+-- This table contains the rating of a movie by a user in their review.
+-- created_at is the user's review date. 
+
+-- Output: 
+-- +--------------+
+-- | results      |
+-- +--------------+
+-- | Daniel       |
+-- | Frozen 2     |
+-- +--------------+
+-- Explanation: 
+-- Daniel and Monica have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+-- Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.
+ 
+
+-- Write a solution to:
+
+-- Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+-- Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+-- The result format is in the following example.
+
+-- # Write your MySQL query statement below
+(SELECT name AS results
+    FROM users AS u
+    LEFT JOIN movierating AS mr
+    ON u.user_id = mr.user_id
+    GROUP BY u.user_id 
+    ORDER BY COUNT(*) DESC, name
+    LIMIT 1
+)
+UNION ALL
+(SELECT title AS results
+    FROM movies AS m
+    LEFT JOIN movierating AS mr
+    ON m.movie_id = mr.movie_id
+    WHERE month(created_at)=2 AND year(created_at)=2020
+    GROUP BY m.movie_id
+    ORDER BY AVG(rating) DESC, title
+    LIMIT 1
+)
